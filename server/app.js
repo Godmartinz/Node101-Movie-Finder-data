c3b49c1d0e35b6e694bb3022d7a04f620cd20409
Godfrey Martinez <godmartinz@gmail.com>
2019-04-14 13:50:04 -0700

    adds initial project files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,34 @@
 const express = require('express');
-
-
+const morgan = require('morgan');
+const axios = require('axios');
+const url="http://www.omdbapi.com/?apikey=8730e0e&'";
 const app = express();
+
+
+var cache={};
+app.use(morgan('dev'));
+app.get('/', (req, res)=> {
+    let MovieKey = req.url;
+
+    if(cache[MovieKey]){
+        console.log('Getting movie from cache');
+        return res.json(cache[MovieKey]);
+    } else {
+        console.log("getting movie from OMDB...", MovieKey);
+        return axios.get('http://www.omdbapi.com'+ MovieKey + '&apikey=8730e0e').then(response => {
+            let MovieData=response.data;
+            console.log('We got the Movie data');
+            cache[MovieKey]=MovieData;
+            return res.json(MovieData);
+        }).catch(err =>{
+            console.log(err);
+            return res.json("Error")
+        })
+    
+
+    }
+
+});
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
